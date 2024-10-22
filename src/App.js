2852587cf6f35eec552ebd85fb619f19bb82b94b
TamasVencel3 <tamas.vencel@accenture.com>
2024-10-22 20:14:40 +0300

    add key to SplitBillForm to reset form inputs

--- src/App.js
+++ src/App.js
@@ -83,6 +83,7 @@ export default function App() {
         <SplitBillForm
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
